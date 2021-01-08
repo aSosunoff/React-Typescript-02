2caa85e50f57971662c524883d3546f533d12182
asosunoff <al.sosunoff@gmail.com>
2021-01-08 16:50:25 +0300

    Added ItemStatusFilter

---
+++ src/components/ItemStatusFilter/ItemStatusFilter.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./ItemStatusFilter.module.scss";
+
+const ItemStatusFilter: React.FC = () => {
+  return (
+    <div className="btn-group">
+      <button type="button" className="btn btn-info">
+        All
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        Active
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        Done
+      </button>
+    </div>
+  );
+};
+
+export default ItemStatusFilter;
