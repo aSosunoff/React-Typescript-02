1adff5c8ce2c6b3c045deb0d6b1cea9266cd1d31
asosunoff <al.sosunoff@gmail.com>
2021-01-08 16:59:20 +0300

    Fix style todo list item

--- src/components/TodoList/TodoListItem.tsx
+++ src/components/TodoList/TodoListItem.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { ITodoListItemProps } from "../Interfaces";
+import styles from "./TodoListItem.module.scss";
 
 const TodoListItem: React.FC<ITodoListItemProps> = ({
   label,
   important = false,
 }) => {
   const style: React.CSSProperties = {
-    color: important ? "tomato" : "black",
+    color: important ? "steelblue" : "black",
+    fontWeight: important ? "bold" : "normal",
   };
 
-  return <span style={style}>{label}</span>;
+  return (
+    <span className={styles.item}>
+      <span className={styles.label} style={style}>
+        {label}
+      </span>
+
+      <button
+        type="button"
+        className="btn btn-outline-success btn-sm float-right"
+      >
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button
+        type="button"
+        className="btn btn-outline-danger btn-sm float-right"
+      >
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
 };
 
 export default TodoListItem;
