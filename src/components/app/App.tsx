baa8009a3a4bd4450cd825f851262b896dc8d9de
asosunoff <al.sosunoff@gmail.com>
2021-01-08 15:18:00 +0300

    fix

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Header from "../AppHeader";
 import { ITodo } from "../Interfaces";
 import SearchPanel from "../SearchPanel";
 import TodoList from "../TodoList";
 
 const App: React.FC = () => {
-  const todoDate: ITodo[] = [
-    { id: Date.now(), label: "Drink Coffe", important: false },
-    { id: Date.now() + 1, label: "Make Awesome App", important: true },
-    { id: Date.now() + 2, label: "Have a lunch", important: false },
-  ];
+  const todoDate: ITodo[] = useMemo<ITodo[]>(
+    () => [
+      { id: Date.now(), label: "Drink Coffe", important: false },
+      { id: Date.now() + 1, label: "Make Awesome App", important: true },
+      { id: Date.now() + 2, label: "Have a lunch", important: false },
+    ],
+    []
+  );
 
   return (
     <div>
