3bc23e77441ac56c3613177d617dcfe609596539
asosunoff <al.sosunoff@gmail.com>
2021-01-08 13:54:52 +0300

    Fix app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return <div className="App">123123</div>;
+  return (
+    <div>
+      <h1>My Todo List</h1>
+      <input placeholder="search" />
+      <ul>
+        <li>Learn React</li>
+        <li>Build Awesome App</li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
