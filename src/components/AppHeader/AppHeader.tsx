7af28a2f7994654a32d07d844fa864151a91b72b
asosunoff <al.sosunoff@gmail.com>
2021-01-08 16:32:42 +0300

    fix app header

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import cn from "classnames";
+import styles from "./AppHeader.module.scss";
 
-const AppHeader: React.FC = () => {
-  return <h1>My Todo List</h1>;
+type AppHeaderType = {
+  todo?: number;
+  done?: number;
+};
+
+const AppHeader: React.FC<AppHeaderType> = ({ done = 0, todo = 0 }) => {
+  return (
+    <div className={cn(styles.header, "d-flex")}>
+      <h1>Todo List</h1>
+      <h2>
+        {todo} more to do, {done} done
+      </h2>
+    </div>
+  );
 };
 
 export default AppHeader;
