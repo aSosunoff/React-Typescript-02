210b0d5041c0e19a85463bdd36b2ae77c94e019e
asosunoff <al.sosunoff@gmail.com>
2021-01-11 10:37:51 +0300

    fix interface

--- src/components/Interfaces.ts
+++ src/components/Interfaces.ts
@@ -1,19 +1,11 @@
-/* import { OmitFromType } from "./utils/UtilityTypes"; */
-
 export type ITodo = {
-    id: number;
-    label: string;
-    important: boolean;
+  id: number;
+  label: string;
+  important: boolean;
 }
 
-type ITodoListItemHandlerProps = {
-    onSetImportantHandler: (id: number) => void,
-    onDeleteHandler: (id: number) => void,
+export type TodoContextType = {
+  todos: ITodo[];
+  setImportantHandler: (id: number) => void;
+  deleteHandler: (id: number) => void;
 };
-
-/* export type ITodoListItemProps = OmitFromType<ITodo, "id"> & ITodoListItemHandlerProps; */
-export type ITodoListItemProps = ITodo & ITodoListItemHandlerProps;
-
-export type ITodoListProps = {
-    todos?: ITodo[];
-} & ITodoListItemHandlerProps;
