2f15b2daae93b7bfd94807c12b33c22556acd65e
asosunoff <al.sosunoff@gmail.com>
2021-01-11 14:05:40 +0300

    Added form

--- src/components/ItemAddForm/ItemAddForm.tsx
+++ src/components/ItemAddForm/ItemAddForm.tsx
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import cn from "classnames";
 import { useTodoContext } from "../../context/todoContext";
 import styles from "./ItemAddForm.module.scss";
 
 const ItemAddForm: React.FC = () => {
+  const [label, setLabel] = useState("");
+
   const { addHandler } = useTodoContext();
+
+  const submitHandler = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      addHandler(label);
+      setLabel("");
+    },
+    [addHandler, label]
+  );
+
+  /* const submitHandler = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const form = new FormData(e.target as HTMLFormElement);
+
+      const { label } = [...form.entries()].reduce<FormType>(
+        (res, [key, value]) => ({
+          ...(res as FormType),
+          [key]: value as string,
+        }),
+        {} as FormType
+      );
+
+      addHandler(label);
+    },
+    [addHandler]
+  ); */
+
+  const inputHandler = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => setLabel(e.target.value),
+    []
+  );
+
   return (
-    <div className={styles.item_add_form}>
-      <button
-        className="btn btn-btn-outline-secondary"
-        onClick={addHandler?.bind(null, "test")}
-      >
-        Добавить
-      </button>
-    </div>
+    <form
+      className={cn("d-flex", styles.item_add_form)}
+      onSubmit={submitHandler}
+    >
+      <input
+        type="text"
+        className="form-control"
+        value={label}
+        onInput={inputHandler}
+      />
+      <button className="btn btn-outline-primary">Добавить</button>
+    </form>
   );
 };
 
